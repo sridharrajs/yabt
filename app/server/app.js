41a08d37be57beee6dbceb4f2ec39a3ac5cd985a
sridhar <me@sridhar.co>
2016-01-08 19:57:45 +0530

    base commit

---
+++ app/server/app.js
@@ -0,0 +1,46 @@
+/**
+ * Created by sridharrajs on 1/6/16.
+ */
+
+'use strict';
+
+let bp = require('body-parser');
+let cors = require('cors');
+let express = require('express');
+let compression = require('compression');
+
+let authFilter = require('./middlewares/auth-filter');
+let reqHeaderFilter = require('./middlewares/request-header');
+
+let app = express();
+
+app.use(cors());
+app.use(compression());
+app.use(bp.json());
+app.use(bp.urlencoded({
+	extended: false
+}));
+
+app.use(reqHeaderFilter.setHeaders);
+app.use(express.static('../client/'));
+app.set('view engine', 'ejs');
+
+app.all('/*', [authFilter.authenticate]);
+
+app.get('/', function (req, res) {
+	res.status(200).send({
+		msg: 'Server is up!'
+	});
+});
+
+process.on('uncaughtException', function (err) {
+	console.error(err.stack);
+});
+
+function getApp() {
+	return app;
+}
+
+module.exports = {
+	getApp
+};
---
+++ config.js
@@ -0,0 +1,51 @@
+/**
+ * Created by sridharrajs on 1/6/16.
+ */
+
+'use strict';
+
+let _ = require('lodash');
+let fs = require('fs');
+
+const ENVS = [
+	'prod',
+	'local'
+];
+
+const CONFIGURATIONS = JSON.parse(fs.readFileSync('./configuration.json', 'utf-8'));
+
+class Config {
+
+	static isValidEnv(HOST_ENV) {
+		return new Promise((resolve, reject)=> {
+			if (_.contains(ENVS, HOST_ENV)) {
+				resolve('Success');
+			} else {
+				reject('Failed');
+			}
+		});
+	}
+
+	static isSecretSet(MY_SECRET) {
+		return new Promise((resolve, reject)=> {
+			if (MY_SECRET) {
+				resolve('Success');
+			} else {
+				reject('Failed');
+			}
+		});
+	}
+
+	static init(HOST_ENVIRONMENT) {
+		this.port = CONFIGURATIONS.port;
+
+		let settings = CONFIGURATIONS[HOST_ENVIRONMENT];
+		this.secure = settings.secure;
+		this.mongdbUrl = settings.mongodb_url;
+		this.secret = process.env.MY_SECRET;
+		return Promise.resolve('Success');
+	}
+
+}
+
+module.exports = Config;
