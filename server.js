9be3f427c625fe17d7ea408412747c11c8e70a98
sridharrajs <me@sridhar.co>
2016-12-07 18:07:11 +0530

    a dry run commit for testing the code formating

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ let config = require('./config');
 let bootSequence = require('./bin/boot-sequence');
 
 bootSequence.load().then((info)=> {
-	console.log('Establishing DB connection ', chalk.blue(info));
-	let appServer = require('./app/boot/build-server');
-	return appServer.start(config);
+    console.log('Establishing DB connection ', chalk.blue(info));
+    let appServer = require('./app/boot/build-server');
+    return appServer.start(config);
 }).then((info)=> {
-	console.log(`Starting Server at ${chalk.green(config.port)}`, chalk.blue(info));
+    console.log(`Starting Server at ${chalk.green(config.port)}`, chalk.blue(info));
 }).catch((error)=> {
-	console.log(chalk.red(error.stack));
-	process.exit(0);
+    console.log(chalk.red(error.stack));
+    process.exit(0);
 });
 
 process.on('uncaughtException', (err) => {
-	console.error(err.stack);
+    console.error(err.stack);
 });
